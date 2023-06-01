bc46cc1be092a918c1b2953e31e3a866abf4d9d9
Gabriela Carniel <gabiicarniel@gmail.com>
2023-06-01 10:08:07 -0300

    initial commit with styles

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,41 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { CLoseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from "@phosphor-icons/react";
+
+
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay/>
+        <Content>
+          <Dialog.Title>Nova transação</Dialog.Title>
+
+          <CLoseButton>
+            <X size={24}/>
+          </CLoseButton>
+
+            
+          <form action="">
+            <input type="text" placeholder='Descrição' required />
+            <input type="number" placeholder='Preço' required />
+            <input type="text" placeholder='Categoria' required />
+
+            <TransactionType>
+              <TransactionTypeButton variant='income'>
+                <ArrowCircleUp size={24}/>
+                Entrada
+              </TransactionTypeButton>
+
+              <TransactionTypeButton variant='outcome'>
+                <ArrowCircleDown size={24}/>
+                Saída
+              </TransactionTypeButton>
+            </TransactionType>
+
+            <button type='submit'>Cadastrar</button>
+          </form>
+      </Content>
+    </Dialog.Portal>
+  )
+}
